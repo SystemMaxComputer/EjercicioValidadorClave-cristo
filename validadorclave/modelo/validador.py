d6960343baaa4b10fdfa3c49e4b98ad1cbbd453a
Cristopher <geronimosanchezromero@gmail.com>
2024-10-17 10:56:04 -0500

    creación de las otras clases con sus metodos aun no definidos

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,8 +1,8 @@
 # TODO: Implementa el código del ejercicio aquí
-from abc import ABC, abstractmethod
+from abc import abstractmethod, ABC
 
 
-class ReglaValidacion[ABC]:
+class ReglaValidacion(ABC):
     def __init__(self, longitud_esperada: int):
         self.longitud_esperada: int = longitud_esperada
 
@@ -12,13 +12,40 @@ class ReglaValidacion[ABC]:
         return False
 
     def _contiene_mayuscula(self, clave: str) -> bool:
-        pass
+        for letra in clave:
+            if letra.isupper():
+                return True
+        return False
 
     def _contiene_minuscula(self, clave: str) -> bool:
-        pass
+        for letra in clave:
+            if letra.islower():
+                return True
+        return False
 
     def _contiene_numero(self, clave: str) -> bool:
         pass
+
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
         pass
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        pass
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class Validador:
+    pass
