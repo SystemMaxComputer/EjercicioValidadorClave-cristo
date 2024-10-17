b07df0e81c60791bcdb8538a5e5aa1b58b488c4f
Cristopher <geronimosanchezromero@gmail.com>
2024-10-17 10:26:43 -0500

    clase ReglaValidación y sus métodos sin definir

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,22 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion[ABC]:
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada: int = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        pass
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_numero(self, clave: str) -> bool:
+        pass
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
