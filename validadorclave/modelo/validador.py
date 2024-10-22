f21fe836ada5961e35b76a8472f768dcfe7dde90
B12_309_11\b12s309 <yajairaivondelriocardona@gmail.com>
2024-10-22 11:34:51 -0500

    corrección y finalización del ejercicio

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,6 +1,9 @@
 # TODO: Implementa el código del ejercicio aquí
 from abc import abstractmethod, ABC
 
+from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLetraMayusculaError, \
+    NoTieneLetraMinusculaError, NoTieneNumeroError, NoTieneCaracterEspecialError, NoTienePalabraSecretaError
+
 
 class ReglaValidacion(ABC):
     def __init__(self, longitud_esperada: int):
@@ -47,8 +50,17 @@ class ReglaValidacionGanimedes(ReglaValidacion):
         return False
 
     def es_valida(self, clave: str) -> bool:
-        pass
-
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 8 caracteres")
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError("La clave debe tener al menos una letra mayúscula")
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError("la clave debe tener al menos una letra minúscula")
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError("La clave debe tener al menos un número")
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError("La clave debe tener al menos un caracter especial (@, _, #, $, %)")
+        return True
 
 class ReglaValidacionCalisto(ReglaValidacion):
     def __init__(self):
@@ -56,16 +68,43 @@ class ReglaValidacionCalisto(ReglaValidacion):
 
     @staticmethod
     def contiene_calisto(clave: str) -> bool:
-        if "calisto" in str.lower(clave):
-            mayusculas = sum(1 for caracter in clave if caracter.isupper())
-            if 2 <= mayusculas < 7:
-                return True
-            return False
+        clave_temp: str = clave.lower()
+        palabra: str = "calisto"
+        index_calisto = 0
+
+        while (index_calisto + len(palabra)) < len(clave):
+            index_calisto = clave_temp.find(palabra, index_calisto)
+            if index_calisto >= 0:
+                pi = index_calisto
+                pf = index_calisto + len(palabra)
+                palabra_org = clave[pi:pf]
+
+                contador_may = 0
+                for letra in palabra_org:
+                    if letra.isupper():
+                        contador_may += 1
+
+                if 2 <= contador_may < len(palabra):
+                    return True
+                else:
+                    index_calisto += len(palabra)
+            else:
+                return False
         return False
 
     def es_valida(self, clave: str) -> bool:
-        pass
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 6 caracteres")
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError("La clave debe tener al menos un número")
+        if not self.contiene_calisto(clave):
+            raise NoTienePalabraSecretaError("La clave debe tener la palabra calisto con al menos dos letras mayúsculas pero no todas")
+        return True
 
 
 class Validador:
-    pass
+    def __init__(self, regla: ReglaValidacion):
+        self.regla: ReglaValidacion = regla
+
+    def es_valida(self, clave) -> bool:
+        return self.regla.es_valida(clave)
